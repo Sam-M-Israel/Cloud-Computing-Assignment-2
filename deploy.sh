1b768015c08af27f0bafd4fe37ac4e4be97fe65a
Sam Morrow <sam-m-israel>
2021-06-26 19:42:43 +0300

    commented unused code

--- deploy.sh
+++ deploy.sh
@@ -69,8 +69,8 @@ Ec2_3_IP=$(aws cloudformation --region $REGION describe-stacks --stack-name "$ST
 STACK_TGROUP=$(aws cloudformation --region $REGION describe-stacks --stack-name shai-elad-stack --query "Stacks[0].Outputs[?OutputKey=='TargetGroup'].OutputValue" --output text)
 
 echo "New instance at $Ec2_1_IP with subnet: $SUBNET_ID_1"
-echo "New instance at $Ec2_2_IP with subnet: $SUBNET_ID_1"
-echo "New instance at $Ec2_3_IP with subnet: $SUBNET_ID_1"
+echo "New instance at $Ec2_2_IP with subnet: $SUBNET_ID_2"
+echo "New instance at $Ec2_3_IP with subnet: $SUBNET_ID_3"
 echo "Waiting for instances to register as healthy"
 sleep 20
 
@@ -82,45 +82,45 @@ echo "$T_HEALTH_CHECK"
 ELB_NAME="SamOmerELB"
 echo "Getting DNS Name for ELB: $ELB_NAME"
 DNS_ADD=$(aws elbv2 describe-load-balancers --names "$ELB_NAME" | jq -r .LoadBalancers[0].DNSName)
-echo $DNS_ADD
+echo "$DNS_ADD"
 
-echo "setup rule allowing SSH access to $MY_IP only"
-aws ec2 authorize-security-group-ingress        \
-    --group-name $SEC_GRP --port 22 --protocol tcp \
-    --cidr $MY_IP/32
-
-echo "setup rule allowing HTTP (port 5000) access to $MY_IP only"
-aws ec2 authorize-security-group-ingress        \
-    --group-name $SEC_GRP --port 5000 --protocol tcp \
-    --cidr $MY_IP/32
-
-UBUNTU_20_04_AMI="ami-08962a4068733a2b6"
-
-echo "Creating Ubuntu 20.04 instance..."
-RUN_INSTANCES=$(aws ec2 run-instances   \
-    --image-id $UBUNTU_20_04_AMI        \
-    --instance-type t2.micro            \
-    --key-name $KEY_NAME                \
-    --security-groups $SEC_GRP)
-
-INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[0].InstanceId')
-
-echo "Waiting for instance creation..."
-aws ec2 wait instance-running --instance-ids $INSTANCE_ID
-
-PUBLIC_IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID |
-    jq -r '.Reservations[0].Instances[0].PublicIpAddress'
-)
-
-echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
-echo "deploying code to production"
-scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" launch_script.sh ubuntu@$PUBLIC_IP:/home/ubuntu/
-
-echo "setup production environment"
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
-  sh -e launch_script.sh
-EOF
-
-echo "test that it all worked"
-curl  --retry-connrefused --retry 10 --retry-delay 1  http://$PUBLIC_IP:5000
+#echo "setup rule allowing SSH access to $MY_IP only"
+#aws ec2 authorize-security-group-ingress        \
+#    --group-name $SEC_GRP --port 22 --protocol tcp \
+#    --cidr $MY_IP/32
+#
+#echo "setup rule allowing HTTP (port 5000) access to $MY_IP only"
+#aws ec2 authorize-security-group-ingress        \
+#    --group-name $SEC_GRP --port 5000 --protocol tcp \
+#    --cidr $MY_IP/32
+#
+#UBUNTU_20_04_AMI="ami-08962a4068733a2b6"
+#
+#echo "Creating Ubuntu 20.04 instance..."
+#RUN_INSTANCES=$(aws ec2 run-instances   \
+#    --image-id $UBUNTU_20_04_AMI        \
+#    --instance-type t2.micro            \
+#    --key-name $KEY_NAME                \
+#    --security-groups $SEC_GRP)
+#
+#INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[0].InstanceId')
+#
+#echo "Waiting for instance creation..."
+#aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+#
+#PUBLIC_IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID |
+#    jq -r '.Reservations[0].Instances[0].PublicIpAddress'
+#)
+#
+#echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
+#echo "deploying code to production"
+#scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" launch_script.sh ubuntu@$PUBLIC_IP:/home/ubuntu/
+#
+#echo "setup production environment"
+#ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
+#  sh -e launch_script.sh
+#EOF
+#
+#echo "test that it all worked"
+#curl  --retry-connrefused --retry 10 --retry-delay 1  http://$PUBLIC_IP:5000
 
